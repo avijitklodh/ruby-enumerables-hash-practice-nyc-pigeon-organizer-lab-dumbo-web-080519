af1394f9819653fbe183c6769f179ab54fcd600c
Avijit Lodh <avijitklodh@gmail.com>
2019-07-28 22:10:10 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,16 +1,17 @@
-def nyc_pigeon_organizer(data)
+def nyc_pigeon_organizer(hash)
   # write your code here!
  final = {}
 
-  data.each do |first_level, all_other|
+  hash.each do |first_level, all_other|
     all_other.each do |category, array|
       array.each do |name|
         final[name] = {:color => [], :gender => [], :lives => []}
       end 
     end 
   end 
+  
   x = final.keys
-  data[:color].each do |bird_color, name|
+  hash[:color].each do |bird_color, name|
     name.each do |bird_name|
       x.each do |item|
         if bird_name === item
@@ -19,7 +20,8 @@ def nyc_pigeon_organizer(data)
       end 
     end 
   end 
-  data[:gender].each do |gender, type|
+ 
+  hash[:gender].each do |gender, type|
     type.each do |bird_name|
       x.each do |item|
         if bird_name === item
@@ -28,7 +30,8 @@ def nyc_pigeon_organizer(data)
       end 
     end 
   end 
-  data[:lives].each do |location, name|
+
+  hash.each do |location, name|
     name.each do |bird_name|
       x.each do |item|
         if bird_name === item
